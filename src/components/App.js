6b6096fadc6806ae1f390f4f22d779f7867d1dc3
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-09-22 13:47:22 +0200

    Setting up innitial code editor functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from './Editor'
 
 const App = () => {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
   return (
     <>
       <div className="pane top-pane">
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor
+          language="xml"
+          name="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          name="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          name="JavaScript"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane">
         <iframe
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
 
-const Editor = () => {
+const Editor = (props) => {
+  const {
+    language,
+    name,
+    value,
+    onChange,
+  } = props
+
+  const handleChange = (editor, data, value) => {
+    onChange(value)
+  }
+
   return (
-    <div>
-      editor
+    <div className="editor-container">
+      <div className="editor-title">
+        {name}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="codemirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true,
+        }}
+      />
     </div>
   )
 }
